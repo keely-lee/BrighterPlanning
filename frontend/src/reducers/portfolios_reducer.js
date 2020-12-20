8fb503ecacaf11b9679bd644d7fad84ecb2129d1
keely_lee <keely_lee@outlook.com>
2020-12-19 20:09:04 -0500

    Edit small errors

--- frontend/src/reducers/portfolios_reducer.js
+++ frontend/src/reducers/portfolios_reducer.js
@@ -4,7 +4,7 @@ const portfoliosReducer = (state = {}, action) => {
   Object.freeze(state);
   switch (action.type) {
     case RECEIVE_PORTFOLIO: 
-      return Object.assign({}, state)
+      return Object.assign({}, state, action.portfolio)
     default:
       return state;
   }
