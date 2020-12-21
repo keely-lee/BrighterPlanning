092d6ca611a0b6768696e5ae51d400381cd73ead
keely_lee <keely_lee@outlook.com>
2020-12-21 00:44:52 -0500

    Prep wrapper to begin phase 1 implementation

---
+++ frontend/src/components/PlansMain.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+function PlansMain(props) {
+
+}
+
+export default PlansMain; 
