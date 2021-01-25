16f040344b169b4a25afb732abf9873d83498800
keely_lee <keely_lee@outlook.com>
2021-01-25 11:36:30 -0500

    Edit blur

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -4,24 +4,27 @@ import { Link } from 'react-router-dom';
 import '../stylesheets/navbar.css';
 
 function Nav(props) {
-  const [contactDropdown, toggleContactDropdown] = useState(false)
+  const [contactDropdown, toggleContactDropdown] = useState(false);
+  
+
   return (
     <nav id="navbar">
       <Link to="/plans"><i className="far fa-lightbulb"></i>BRIGHTER PLANNING</Link>
       <div className="nav-contact-wrapper">
         <button type="button" 
           className="nav-contact-button"
-          onClick={() => toggleContactDropdown(true)} 
+          onFocus={() => toggleContactDropdown(true)} 
           onBlur={() => {
+
             toggleContactDropdown(false)
           }}
         >Contact Us</button>
         { contactDropdown ? 
           <div className="nav-contact">
             <p>Keely Lee</p>
-            <a href="mailto:keely_lee@outlook.com"><i className="fas fa-paper-plane"></i>keely_lee@outlook.com</a>
+            <a href="mailto:keely_lee@outlook.com" ><i className="fas fa-paper-plane"></i>keely_lee@outlook.com</a>
             <div className="nav-contact-links">
-              <a href="https://www.linkedin.com/in/keely-lee1/" className="linkedin" ><i className="fab fa-linkedin"></i></a>
+              <a href="https://www.linkedin.com/in/keely-lee1/" className="linkedin" target="_blank" rel="noreferrer"><i className="fab fa-linkedin"></i></a>
               <a href="https://github.com/keely-lee" className="github" ><i className="fab fa-github"></i></a>
               <a href="https://angel.co/u/keely-lee" className="angellist" ><i className="fab fa-angellist"></i></a>
               <a href="https://keely-lee.github.io/" className="personal" ><i className="fas fa-user-circle"></i></a>
